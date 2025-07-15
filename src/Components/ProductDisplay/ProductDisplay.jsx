037d702f5bab3d62022e289badfa858e462beb4c
irmapki <irmanct127@gmail.com>
2025-07-15 23:19:04 +0700

    2.30 mulai memasuki logika add to cart

--- src/Components/ProductDisplay/ProductDisplay.jsx
+++ src/Components/ProductDisplay/ProductDisplay.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './ProductDisplay.css'
 import star_icon from "../Assets/star_icon.png";
 import star_dull_icon from "../Assets/star_dull_icon.png";
+import { ShopContext } from '../../Context/ShopContext';
 
 const ProductDisplay = (props) => {
-  const {product} = props;
+  const { product } = props;
+  const { addToCart } = useContext(ShopContext);
+
   return (
     <div className='productdisplay'>
       <div className="productdisplay-left">
@@ -16,42 +19,52 @@ const ProductDisplay = (props) => {
         </div>
         <div className="productdisplay-img">
           <img className='productdisplay-main-img' src={product.image} alt="" />
+        
         </div>
       </div>
-    <div className="productdisplay-right">
-      <h1>{product.name}</h1>
-      <div className="productdisplay-right-stars">
-        <img src={star_icon} alt="" />
-        <img src={star_icon} alt="" />
-        <img src={star_icon} alt="" />
-        <img src={star_icon} alt="" />
-        <img src={star_dull_icon} alt="" />
-        <p>(122)</p>
-      </div>
-      <div className="productdisplay-right-prices">
-        <div className="productdisplay-right-price-old">${product.old_price}</div>
-      <div className="productdisplay-right-price-new">${product.new_price}</div>
-      <div className="productdisplay-right-description">
-      Ini adalah baju keren mantap jiwa. pernah dipakai pak prabowo dan gibran dan ini sangat amat sangat langka karena hanya ada 1 didunia ini
-      </div>
-      <div className="productdisplay-right-size">
-        <h1>Select Size</h1>
-        <div className="productdisplay-right-sizes">
-          <div>S</div>
-          <div>M</div>
-          <div>L</div>
-          <div>XL</div>
-          <div>XXL</div>
-        </div>
-      </div>
-      <button>ADD TO CART</button>
-      <p className='productdisplay-right-category'><span>Category :</span>Women, T-Shirt, Crop Top</p>
-      <p className='productdisplay-right-category'><span>Tags :</span>Modern, Latest</p>
 
+      <div className="productdisplay-right">
+        <h1>{product.name}</h1>
+
+        <div className="productdisplay-right-stars">
+          <img src={star_icon} alt="" />
+          <img src={star_icon} alt="" />
+          <img src={star_icon} alt="" />
+          <img src={star_icon} alt="" />
+          <img src={star_dull_icon} alt="" />
+          <p>(122)</p>
+        </div>
+
+        <div className="productdisplay-right-prices">
+          <div className="productdisplay-right-price-old">${product.old_price}</div>
+          <div className="productdisplay-right-price-new">${product.new_price}</div>
+        </div>
+
+        <div className="productdisplay-right-description">
+          Ini adalah baju keren mantap jiwa. Pernah dipakai Pak Prabowo dan Gibran, dan ini sangat amat langka karena hanya ada 1 di dunia ini.
+        </div>
+
+        <div className="productdisplay-right-size">
+          <h1>Select Size</h1>
+          <div className="productdisplay-right-sizes">
+            <div>S</div>
+            <div>M</div>
+            <div>L</div>
+            <div>XL</div>
+            <div>XXL</div>
+          </div>
+        </div>
+
+        <button onClick={() => addToCart(product.id)}>ADD TO CART</button>
+        <p className='productdisplay-right-category'>
+          <span>Category :</span> Women, T-Shirt, Crop Top
+        </p>
+        <p className='productdisplay-right-category'>
+          <span>Tags :</span> Modern, Latest
+        </p>
       </div>
-    </div>
     </div>
   )
 }
 
-export default ProductDisplay
+export default ProductDisplay;
